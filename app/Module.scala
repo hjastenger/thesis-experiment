a4c37514b08fd90159b01d3e5a6e07dda86094fc
Hector Stenger <github@orti.works>
2018-06-01 11:20:25 +0200

    fix(waste): remove unused routes, controllers and templates

--- app/Module.scala
+++ app/Module.scala
@@ -1,12 +1,6 @@
 import com.google.inject.AbstractModule
-import play.api.libs.concurrent.AkkaGuiceSupport
 
-class Module extends AbstractModule with AkkaGuiceSupport {
-  import actors._
 
   override def configure(): Unit = {
-    bindActor[StocksActor]("stocksActor")
-    bindActor[UserParentActor]("userParentActor")
-    bindActorFactory[UserActor, UserActor.Factory]
   }
 }
